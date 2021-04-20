d48c60bc48dde2dfebe8dcb480c3a8b0ebda4b1c
Diego Souza <dee-souza@hotmail.com>
2021-04-20 18:23:08 -0300

    Add server json to episodes api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,3 +10,14 @@ export default function Home() {
     </div>
   )
 }
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data
+    }
+  }
+}
