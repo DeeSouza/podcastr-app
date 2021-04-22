4c0de69b560178fc934e847abe84ae4f630c1982
Diego Souza <dee-souza@hotmail.com>
2021-04-22 09:54:16 -0300

    Configure api server and format episodes in page index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,87 @@
-import Head from 'next/head'
+import { GetStaticProps } from 'next';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
-export default function Home() {
+import styles from './home.module.scss';
+
+type FileEpisode = {
+  duration: string;
+  url: string;
+}
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  published_at: string;
+  thumbnail: string;
+  description: string;
+  publishedAt: string;
+  duration: number;
+  durationAsString: string;
+  url: string;
+  file: FileEpisode;
+}
+
+type HomeProps = {
+  latestEpisodes: Episode[],
+  allEpisodes: Episode[],
+}
+
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
   return (
-    <div>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+
+        <ul>
+          {latestEpisodes.map(episode => {
+            return (
+              <li key={episode.id}>
+                <a href="">{episode.title}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+
+      </section>
     </div>
   )
 }
 
-export async function getServerSideProps() {
-  const response = await fetch('http://localhost:3333/episodes')
-  const data = await response.json();
+export const getServerSideProps: GetStaticProps = async () => {
+  const { data } = await api.get<Episode[]>('episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  });
+
+  const episodes = data.map(episode => {
+    return {
+      ...episode,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+        locale: ptBR
+      }),
+      url: episode.file.url,
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+    }
+  });
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
 
   return {
     props: {
-      episodes: data
-    }
+      allEpisodes,
+      latestEpisodes
+    },
   }
 }
